a086395815ebbed753a46354dcfdc8bdc836cc99
David Inoa <davidinoa@gmail.com>
2018-01-22 11:22:25 -0500

    Add a stopwatch to the scoreboard

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Stats from './Stats.jsx';
+import Stopwatch from './Stopwatch.jsx';
 
 const Header = (props) => (
   <div className="header">
     <Stats players={props.players}/>
-    <h1> {props.title} </h1>    
+    <h1> {props.title} </h1>
+    <Stopwatch />    
   </div>
 );
 
