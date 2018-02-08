36728baf9ea53bb3422bf6de5719b82fe3e134fe
David Inoa <davidinoa@gmail.com>
2018-02-08 14:46:41 -0500

    Define action types

--- client/src/components/Scoreboard.jsx
+++ client/src/components/Scoreboard.jsx
@@ -4,17 +4,15 @@ import AddPlayerForm from './AddPlayerForm.jsx';
 import Header from './Header.jsx';
 import Player from './Player.jsx';
 
-const PLAYERS = [
-  { name: 'David Inoa', score: 17, id: 1 },
-  { name: 'Efrain Marquez', score: 13, id: 2 },
-  { name: 'Joe Neckles', score: 14, id: 3 }
-];
-
 class Scoreboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      players: PLAYERS,
+      players: [
+        { name: 'David Inoa', score: 17, id: 1 },
+        { name: 'Efrain Marquez', score: 13, id: 2 },
+        { name: 'Joe Neckles', score: 14, id: 3 }
+      ],
       nextId: 4
     };
   }
